ee08ae11644231e96b753e99017514b2b80ead91
Peter Loft <peterloft3@gmail.com>
2022-02-17 14:11:20 -0600

    finished routes, first round

--- routes/companies.js
+++ routes/companies.js
@@ -8,12 +8,12 @@ const router = express.Router();
 /** Get route to return all companies' information 
  * {companies: [{code, name}, ...]}
 */
-router.get("/", async function (req, resp, next) {
+router.get("/", async function (req, res, next) {
   const companyQuery = await db.query(
     'SELECT code, name FROM companies'
   );
   const companies = companyQuery.rows;
-  return resp.json({ companies });
+  return res.json({ companies });
 
 });
 
@@ -32,7 +32,7 @@ router.get("/:code", async function (req, res, next) {
   );
 
   const company = companyQuery.rows[0];
-  if (!company) throw new NotFoundError();
+  if (!company) throw new NotFoundError(`No matching company at code: ${code}`);
   return res.json({ company });
 });
 
@@ -53,5 +53,47 @@ router.post("/", async function (req, res, next) {
 
   return res.json({ company });
 })
+
+/**
+ * Edit existing company.
+ * Should return 404 if company cannot be found.
+ * Needs to be given JSON like: {name, description}
+ * Returns update company object: {company: {code, name, description}}
+ */
+router.put("/:code", async function (req, res, next) {
+  const { name, description } = req.body;
+  const code = req.params.code;
+
+  const result = await db.query(`
+  UPDATE companies
+    SET name = $1,
+        description = $2
+    WHERE code = $3
+    RETURNING code, name, description
+  `, [name, description, code]);
+
+  const company = result.rows[0];
+  if (!company) throw new NotFoundError(`No matching company at code: ${code}`);
+
+  return res.json({ company });
+});
+
+/**
+ * Deletes company.
+ * Should return 404 if company cannot be found.
+ * Returns {status: "deleted"}
+ */
+router.delete("/:code", async function (req, res, next) {
+  const code = req.params.code;
+
+  const result = await db.query(
+    `DELETE FROM companies WHERE code = $1 RETURNING code`,
+    [code],
+    );
+    const company = result.rows[0];
+    if (!company) throw new NotFoundError(`No matching company at code: ${code}`);
+
+    return res.json({message: "Deleted"});
+});
 
 module.exports = router;
